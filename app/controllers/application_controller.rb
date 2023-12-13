aac2dd5ee97f66c2fe9724d36df5f32d6df841e4
Sam Tran <samuelttran@gmail.com>
2023-12-12 17:07:06 -0700

    Did too many things...was easier than what I attempted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
+  # def current_user?
+  #   current_user.present?
+  # end
+
   private
 
   def error_message(errors)
